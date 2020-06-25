f9becf2ccaa648b4376284c9a9822e6e31b95975
Azowarg <azowarg@gmail.com>
2020-06-25 23:31:33 +0300

    Make commits in english

--- module_0/skillfactory_module_0.py
+++ module_0/skillfactory_module_0.py
@@ -2,22 +2,28 @@ import numpy as np
 
 
 def score_game(game_core):
-    """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
+    """Run the game 1000 times to find out how fast the game guesses the number"""
+
     count_ls = []
-    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    np.random.seed(1)  # Set RANDOM SEED, so that your experiment can be replicated
+
     random_array = np.random.randint(1, 101, size=1000)
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+
+    print(f"Your algorithm guesses the number in mean of {score} attempts")
     return score
 
 
 def game_core_v3(number):
-    """First set any random number. """
+    """First set any random number. Then by comparing it with our
+    "unknown" number, sets upper and lower limits for next random number"""
+
     count = 1
     lower_limit = 1
     upper_limit = 101
+
     predict = np.random.randint(lower_limit, upper_limit)
     while number != predict:
         count += 1
@@ -26,4 +32,7 @@ def game_core_v3(number):
         elif number < predict:
             upper_limit = predict
         predict = np.random.randint(lower_limit, upper_limit)
-    return count  # выход из цикла, если угадали
+
+    return count  # Out of loop, if it guessed
+
+score_game(game_core_v3)
